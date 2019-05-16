3a86a6484b346dc3ed3cb8b598ec098a4e0ddfc1
venkatesh <venkatesh@venkatesh.local>
2019-05-17 01:18:46 +0530

    component done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
 import React, { Component } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Main from './components/Main';
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+} from 'reactstrap';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      date: new Date().toISOString().split('T')[0],
+    }
+  }
+
+  handleChange(e) {
+    debugger
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
   render() {
+    const { date, description } = this.state;
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <Container fluid>
+        <Row>
+          <Col sm={12}>
+            <Form>
+              <FormGroup>
+                <Label for="exampleEmail">DATE:</Label>
+                <Input
+                  onChange={(e) => this.handleChange(e)}
+                  value={date}
+                  className='font-size'
+                  type="date"
+                  name="date" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="exampleEmail">DESCRIPTION:</Label>
+                <Input
+                  value={description} className='font-size'
+                  type="textarea"
+                  rows={10}
+                  name="date" />
+              </FormGroup>
+            </Form>
+            <Button color="secondary" size="sm" block>SAVE</Button>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
-import store from './store';
 
 ReactDOM.render(
-<BrowserRouter>
-  <Provider store={store}>
-    <App />
-  </Provider>
-</BrowserRouter>, document.getElementById('root'));
+    <App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
